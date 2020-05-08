a0b5fea5b2185593eb2d6073a929b28d2e88461e
小雨 <614422099@QQ.com>
2020-05-08 14:56:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -107,6 +107,7 @@ else
     PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
     COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
     if [ "COMMENTS_URL" != null ]; then
+    	echo "Not file need format"
     	# Pause invalid tips
     	# curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
     fi
