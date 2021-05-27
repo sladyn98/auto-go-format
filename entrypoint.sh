c12db81450743c7b8faa099d3910ffff0677412a
Jorge Alberto Diaz Orozco <jorge.diaz@jimdo.com>
2021-05-27 11:27:57 +0200

    Remove no changes notification

--- entrypoint.sh
+++ entrypoint.sh
@@ -83,12 +83,5 @@ if [[ `git status --porcelain` ]]; then
     if [ "COMMENTS_URL" != null ]; then
     curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
     fi
-else
-    COMMENT=":heavy_check_mark: That is a perfectly formatted change."
-    PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-    COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-    if [ "COMMENTS_URL" != null ]; then
-    curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
-    fi
 fi
 exit $SUCCESS
